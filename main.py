c2e78d46182364c41e85bff6c2809e5925b700b2
Iuri Almeida <ialmeida@vertigo.com.br>
2021-11-09 15:41:02 -0300

    Configurando o ambiente

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+    Projeto: Jogo de Xadrez em Python
+
+    Autor: Iuri Lopes Almeida
+
+    GitHub: https://github.com/Iuri-Almeida
+
+    Objetivo: Recriar o jogo de xadrez em Python rodando pelo terminal.
+
+    Referência: https://github.com/Iuri-Almeida/chess-system-java
+"""
+
+
+def main() -> None:
+    print('Hello world!')
+
+
+if __name__ == '__main__':
+    main()
